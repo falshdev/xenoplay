0e9c30f6d199805e1c081c5e941394c144ac719a
falshdev <mail.vikashdhakar@gmail.com>
2026-03-01 15:33:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOWNLOAD TIMER
 const btn = document.getElementById("downloadBtn");
 const timerText = document.getElementById("timerText");
 const servers = document.getElementById("servers");
@@ -7,16 +8,27 @@ btn.addEventListener("click", () => {
   btn.style.opacity = "0.6";
 
   let timeLeft = 10;
-  timerText.classList.remove("hidden");
+  timerText.innerText = `Preparing download... ${timeLeft}s`;
 
   const interval = setInterval(() => {
-    timerText.innerText = `Preparing download... ${timeLeft}s`;
     timeLeft--;
+    timerText.innerText = `Preparing download... ${timeLeft}s`;
 
-    if (timeLeft < 0) {
+    if (timeLeft <= 0) {
       clearInterval(interval);
       timerText.innerText = "Download ready!";
-      servers.classList.remove("hidden");
+      servers.style.display = "block";
     }
   }, 1000);
+});
+
+// THEME TOGGLE
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  body.classList.toggle("light");
+
+  toggle.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
 });
